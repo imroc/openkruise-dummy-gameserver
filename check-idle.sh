a219ff4e8604335971fd412c1893a381e244c3d9
roc <roc@imroc.cc>
2024-09-30 14:29:35 +0800

    update at 2024-09-30 14:29:35

--- check-idle.sh
+++ check-idle.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# 发送请求并获取状态码
-status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/api/idle)
+result=$(curl http://127.0.0.1:8080/api/idle)
 
-# 检查状态码是否为200
-if [ "$status_code" -eq 200 ]; then
+if [ "$result" -eq "true" ]; then
   exit 0
 else
   exit 1
